b4c701153f23031184b4fceb6668c58fbf8fe11d
kysth0707 <kysth0707@naver.com>
2022-08-22 21:42:09 +0900

    유동적 이미지 크기 변경

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from tkinter import YView
+from PIL import Image
 import pygame
-from pygame.math import Vector2
 import os
+import time
 
 
 def ReturnPos(loc : str):
 	return os.getcwd() + loc
 
-def AddFontText(Text, Alias, Color):
-	global FontTexts, FontRects
-	FontTexts.append(MyFont.render("불러오기", True, FontColor))
-	FontRects.append(FontTexts[len(FontTexts) - 1].get_rect())
-	# print(FontRects[len(FontRects) - 1])
+# def AddFontText(Text, Alias, Color):
+# 	global FontTexts, FontRects
+# 	FontTexts.append(MyFont.render("불러오기", True, FontColor))
+# 	FontRects.append(FontTexts[len(FontTexts) - 1].get_rect())
+# 	# print(FontRects[len(FontRects) - 1])
 	
 
 
@@ -21,13 +22,15 @@ ScreenWidth = 1000
 ScreenHeight = 800
 screen = pygame.display.set_mode((ScreenWidth, ScreenHeight), pygame.RESIZABLE)
 
-MyFont = pygame.font.Font(ReturnPos("\\font\\NEXONLv1GothicRegular.ttf"), 20)
 
-FontColor = (255, 255, 255)
-FontTexts = []
-FontRects = []
-AddFontText("불러오기", True, FontColor)
-AddFontText("저장하기", True, FontColor)
+# MyFont = pygame.font.Font(ReturnPos("\\font\\NEXONLv1GothicRegular.ttf"), 20)
+
+# FontColor = (255, 255, 255)
+# FontTexts = []
+# FontRects = []
+# AddFontText("불러오기", True, FontColor)
+# AddFontText("저장하기", True, FontColor)
+
 
 
 BoxOnColor = (255, 255, 255)
@@ -35,6 +38,22 @@ BoxOffColor = (0, 0, 0)
 BoxNum = 41
 BoxFillSize1 = 50 # y 50 ~ 950
 BoxFillSize2 = 950 #
+
+ImgSelected = None
+
+def GetImageSize():
+	return int(ScreenHeight / 1000 * 100)
+
+def ImageResize():
+	global ImgSelected
+	a = Image.open(ReturnPos("\\img\\original\\Selected.png"))
+	SizeValue = GetImageSize()
+	b = a.resize((SizeValue, SizeValue))
+	b.save(ReturnPos("\\img\\edited\\Selected.png"))
+	ImgSelected = pygame.image.load(ReturnPos("\\img\\edited\\Selected.png"))
+
+ImageResize()
+
 
 def DrawBoxs():
 	BoxSize = (BoxFillSize2 - BoxFillSize1) / BoxNum * ScreenHeight / 1000
@@ -50,11 +69,12 @@ def DrawBoxs():
 												BoxFillSize1  * ScreenHeight / 1000 + loopy * BoxSize * ZoomValue + Y, 
 												BoxSize * ZoomValue, BoxSize * ZoomValue], 2)
 
-def DrawTexts():
-	for loopy in range(len(FontTexts)):
-		Xval = ScreenWidth - 50 - FontRects[0][2]
-		Yval = 50 + loopy * (FontRects[loopy][3] + 20)
-		screen.blit(FontTexts[loopy], (Xval, Yval))
+def DrawIcons():
+	ImageList = [ImgSelected]
+	for i in range(len(ImageList)):
+		Xval = ScreenWidth - GetImageSize()
+		Yval = (50 + i * GetImageSize() + 50) * ScreenHeight / 1000
+		screen.blit(ImageList[i], (Xval, Yval))
 
 def ReturnBoxNum(MousePos):
 	Exit = False
@@ -85,11 +105,14 @@ Run = True
 while Run:
 	ScreenWidth, ScreenHeight = pygame.display.get_surface().get_size()
 
+
 	screen.fill((50,50,50))
 	DrawBoxs()
-	DrawTexts()
+	DrawIcons()
 
 	pygame.display.update()
+
+
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -128,6 +151,11 @@ while Run:
 		elif event.type == pygame.MOUSEBUTTONUP:
 			if event.button == 1:
 				Draging = False
+
+		elif event.type == pygame.VIDEORESIZE:
+			ScreenWidth, ScreenHeight = pygame.display.get_surface().get_size()
+			ImageResize()
+			# print("Resize")
 		
 	if Draging:
 		ChangeValue = MouseLastPos[0] - pygame.mouse.get_pos()[0], MouseLastPos[1] - pygame.mouse.get_pos()[1]
